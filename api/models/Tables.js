049f0fff562a3d625cc7e390c3691a31bc1a263a
Santiagoalvarez2022 <santiagoalvarez290401@gmail.com>
2023-05-18 22:23:00 -0300

    creacion de modelos, relaciones y servidor

---
+++ api/models/Tables.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Tables', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue : DataTypes.UUIDV4
+    },
+    number: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      unique: true
+    },
+
+    
+  },
+  {timestamps: false}//elimina las propiedades de fecha de creacion y de modificacion
+  );
+};
